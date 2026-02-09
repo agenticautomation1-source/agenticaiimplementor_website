3664818e61b9155a2431b0e035d35d388d69111c
agenticautomation1-source <agenticautomation1@gmail.com>
2026-02-10 00:47:47 +0530

    Update LearningPath.page.tsx

--- src/pages/learning-path/LearningPath.page.tsx
+++ src/pages/learning-path/LearningPath.page.tsx
@@ -2,7 +2,6 @@ import "./learning-path.css";
 
 import LPHeader from "./components/LPHeader";
 import LPHero from "./components/LPHero";
-import LPFocusGrid from "./components/LPFocusGrid";
 import LPProgramFlow from "./components/LPProgramFlow";
 import LPEntryPoints from "./components/LPEntryPoints";
 import LPCTA from "./components/LPCTA";
@@ -13,7 +12,6 @@ export default function LearningPathPage() {
     <div data-learning-path className="min-h-screen bg-[#050505] text-slate-100">
       <LPHeader />
       <LPHero />
-      <LPFocusGrid />
       <LPProgramFlow />
       <LPEntryPoints />
       <LPCTA />
